b90437a4cc33aab92acf361d9dd05c8a03b46655
Jason Susanto <jasonsusanto17112003@gmail.com>
2024-05-06 14:03:49 +0700

    Initial Commit

---
+++ object-detection-[Beginner]/object_detection__Beginner_App.swift
@@ -0,0 +1,17 @@
+//
+//  object_detection__Beginner_App.swift
+//  object-detection-[Beginner]
+//
+//  Created by Jason Susanto on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct object_detection__Beginner_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
